f3c0508c40e0d9dda69df98b882e317f65ad7061
Makino Kengo <ognek.m@gmail.com>
2017-09-14 22:55:33 +0900

    ボタンと衝突判定の実装

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		// OutAreaとの衝突判定
+		if (coll.gameObject.tag == "OutArea")
+		{
+			// ゲームマネージャを取得
+			GameObject gameManager = GameObject.Find("GameManager");
+			// リトライ
+			gameManager.GetComponent<GameManager>().PushRetryButton();
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public int StageNo;
+	public bool isBallMoving;
+	public GameObject ballPrefab;
+	public GameObject ball;
+
+	public GameObject goButton;
+	public GameObject retryButton;
+
+	// Use this for initialization
+	void Start () {
+		retryButton.SetActive(false);
+		isBallMoving = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PushGoButton(){
+		// ボールの重力を有効化
+		Rigidbody2D rd = ball.GetComponent<Rigidbody2D>();
+		rd.isKinematic = false;
+
+		retryButton.SetActive(true);
+		goButton.SetActive(false);
+		isBallMoving = true;
+	}
+
+	public void PushRetryButton(){
+		Destroy(ball);
+
+		ball = (GameObject)Instantiate(ballPrefab);
+		goButton.SetActive(true);
+		retryButton.SetActive(false);
+		isBallMoving = false;
+	}
+}
