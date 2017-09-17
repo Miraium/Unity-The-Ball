e179c27eff61de08602816feabb1a6533bb64725
Makino Kengo <ognek.m@gmail.com>
2017-09-18 00:12:59 +0900

    ステージセレクトの実装

---
+++ Assets/Scripts/StageSelectManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelectManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PushStageSelectButton(int stageNo){
+		// 指定された番号のゲームシーンへ
+		SceneManager.LoadScene("PuzzleScene"+stageNo);
+	}
+}
