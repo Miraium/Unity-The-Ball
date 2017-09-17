7adcfb75cf2171b9d3c7b86f149cdcf0982c444a
Makino Kengo <ognek.m@gmail.com>
2017-09-18 00:34:46 +0900

    ステージクリアで次ステージが選択できるように実装

--- Assets/Scripts/StageSelectManager.cs
+++ Assets/Scripts/StageSelectManager.cs
@@ -2,12 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StageSelectManager : MonoBehaviour {
+	// ステージ選択用のボタンを配列で管理
+	public GameObject[] stageButtons;
 
 	// Use this for initialization
 	void Start () {
-		
+		// どのステージまでクリアしているのかをロードする
+		// デフォルト値は0としておく．(セーブ前なら0が代入される)
+		int clearStageNo = PlayerPrefs.GetInt("CLEAR", 0);
+
+		// ステージボタンを有効化
+		for (int i = 0; i <= stageButtons.GetUpperBound(0); i++)
+		{
+			bool b;
+			if (clearStageNo < i)
+			{
+				// 前ステージをクリアしていないのなら無効
+				b = false;
+			}else
+			{
+				// 前ステージをクリアしているのなら有効
+				b = true;
+			}
+
+			// ボタンに対して設定
+			stageButtons[i].GetComponent<Button>().interactable = b;
+		}
 	}
 	
 	// Update is called once per frame
